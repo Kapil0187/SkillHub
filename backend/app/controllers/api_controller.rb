2dec8547c9046df9e62959d11753653d08e91629
Kapil <kapilbarsker2003@gmail.com>
2025-11-19 10:25:42 +0530

    added concern for error handling

--- backend/app/controllers/api_controller.rb
+++ backend/app/controllers/api_controller.rb
@@ -1,8 +1,6 @@
 class ApiController < ActionController::API
   include Pundit::Authorization 
-  rescue_from JWT::DecodeError, with: :render_unauthorized
-  rescue_from JWT::ExpiredSignature, with: :render_token_expired
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized  
+  include ErrorHandler
 
   private
 
@@ -14,7 +12,11 @@ class ApiController < ActionController::API
   end
 
   def current_user
-    @current_user
+    @current_user = @current_user || begin
+      token = token_from_header
+      payload = JsonWebToken.decode(token)
+      User.find_by(id: payload[:user_id])
+    end
   end
 
   def token_from_header
@@ -24,17 +26,5 @@ class ApiController < ActionController::API
     else
       raise JWT::DecodeError, 'Missing or malformed Authorization header'
     end
-  end
-
-  def render_unauthorized(e = nil)
-    render json: { error: 'Unauthorized', message: e&.message }, status: :unauthorized
-  end
-
-  def render_token_expired
-    render json: { error: 'TokenExpired', message: 'Access token expired' }, status: :unauthorized
-  end
-
-  def user_not_authorized
-    render json: { error: "Forbidden" }, status: :forbidden
   end
 end
---
+++ backend/app/controllers/concerns/error_handler.rb
@@ -0,0 +1,28 @@
+module ErrorHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from JWT::DecodeError, with: :render_unauthorized
+    rescue_from JWT::ExpiredSignature, with: :render_token_expired
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized  
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  end
+
+  private
+
+  def render_unauthorized(e = nil)
+    render json: { error: 'Unauthorized', message: e&.message }, status: :unauthorized
+  end
+
+  def render_token_expired
+    render json: { error: 'TokenExpired', message: 'Access token expired' }, status: :unauthorized
+  end
+
+  def user_not_authorized
+    render json: { error: "You are not authorized to perform this action" }, status: :forbidden
+  end
+
+  def record_not_found
+    render json: { error: "Record not found" }, status: :not_found
+  end
+end
